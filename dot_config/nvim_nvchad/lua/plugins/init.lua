189ab6310a695d1dca037ce8853bfa8a09373f98
kenis0225 <ken.huang1@ecoflow.com>
2026-05-03 15:46:04 +0800

    Add telescope.nvim configuration options

--- dot_config/nvim_nvchad/lua/plugins/init.lua
+++ dot_config/nvim_nvchad/lua/plugins/init.lua
@@ -33,4 +33,19 @@ return {
       { "nvim-lua/plenary.nvim", lazy = true },
     },
   },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+      opts = opts or require "nvchad.configs.telescope"
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+        cache_picker = {
+          num_pickers = 3, -- 保留最近几个 picker；-1 = 本会话全保留
+          limit_entries = 2000, -- 每个 picker 最多缓存多少条结果
+          ignore_empty_prompt = true, -- 空 prompt 就关的不进缓存
+        },
+      })
+      return opts
+    end,
+  },
 }
